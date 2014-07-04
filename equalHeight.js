3ce226aeab5ec2dd0d382355af12981007ec799d
Florian Körner <contact@florian-koerner.eu>
2014-07-04 15:42:35 +0200

    Add: destroy method

--- equalHeight.js
+++ equalHeight.js
@@ -1,47 +1,83 @@
 (function($) {
-    $.fn.equalHeight = function() {
-        var $this = $(this),
-            calculateEqualHeights = function() {
-                var equalHeightGroups = {};
+    var EqualHeight = {
+        elements: $(),
+        bindEvents: function() {
+            var self = this;
 
-                // Responsive float-Fix
-                $this.height(1);
+            $(window).on('resize', self, self.eventCallbacks.onWindowResize);
+        },
+        eventCallbacks: {
+            onWindowResize: function(e) {
+                var self = e.data;
 
-                // Group Elements by offset top
-                $this.each(function() {
-                    var y = $(this).offset().top;
+                self.calculateEqualHeights();
+            }
+        },
+        calculateEqualHeights: function() {
+            var self = this;
+            var equalHeightGroups = {};
 
-                    equalHeightGroups[y] = equalHeightGroups[y] || $();
-                    equalHeightGroups[y] = equalHeightGroups[y].add(this);
+            // Responsive float-Fix
+            self.elements.height(1);
+
+            // Group Elements by offset top
+            self.elements.each(function() {
+                var y = $(this).offset().top;
+
+                equalHeightGroups[y] = equalHeightGroups[y] || $();
+                equalHeightGroups[y] = equalHeightGroups[y].add(this);
+            });
+
+            $.each(equalHeightGroups, function(y, elements) {
+                var height = 0;
+
+                // Reset element height to `auto`
+                elements.css('height', 'auto');
+
+                // Find the max height
+                elements.each(function() {
+                    var outerHeight = $(this).outerHeight();
+
+                    if (outerHeight > height) {
+                        height = outerHeight;
+                    }
                 });
 
-                $.each(equalHeightGroups, function(y, elements) {
-                    var height = 0;
+                // Calculate the new height of each element (height without padding and border)
+                elements.each(function() {
+                    var self = $(this),
+                        negative = self.outerHeight() - self.height();
 
-                    // Reset element height to `auto`
-                    elements.css('height', 'auto');
-
-                    // Find the max height
-                    elements.each(function() {
-                        var outerHeight = $(this).outerHeight();
-
-                        if (outerHeight > height) {
-                            height = outerHeight;
-                        }
-                    });
-
-                    // Calculate the new height of each element (height without padding and border)
-                    elements.each(function() {
-                        var self = $(this),
-                            negative = self.outerHeight() - self.height();
-
-                        self.height(height - negative);
-                    });
+                    self.height(height - negative);
                 });
-            };
+            });
+        },
+        add: function(elements) {
+            var self = this;
 
-        // Bind event and calculate initial box dimensions
-        $(window).on('resize', calculateEqualHeights);
-        calculateEqualHeights();
-    }
+            self.remove(elements);
+            self.elements = self.elements.add(elements);
+
+            self.calculateEqualHeights();
+        },
+        remove: function(elements) {
+            var self = this;
+
+            self.elements = self.elements.not(elements);
+        }
+    };
+
+    $.fn.equalHeight = (function(EqualHeight) {
+        EqualHeight.bindEvents();
+
+        return function(method) {
+            switch (method) {
+                case 'destroy':
+                    EqualHeight.remove(this);
+                    break;
+                default:
+                    EqualHeight.add(this);
+            }
+        };
+    }(EqualHeight));
 }(jQuery));
